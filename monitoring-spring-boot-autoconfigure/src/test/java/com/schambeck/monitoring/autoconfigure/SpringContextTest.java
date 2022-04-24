05e8e07e226c3beced0bda06fcc26520027a74c4
Marcelo Schambeck <marcelo.demoraesschambeck@thomsonreuters.com>
2022-04-24 02:25:05 -0300

    Context test

--- monitoring-spring-boot-autoconfigure/src/test/java/com/schambeck/monitoring/autoconfigure/SpringContextTest.java
+++ monitoring-spring-boot-autoconfigure/src/test/java/com/schambeck/monitoring/autoconfigure/SpringContextTest.java
@@ -1,14 +1,34 @@
 package com.schambeck.monitoring.autoconfigure;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@ContextConfiguration(classes = HttpTraceActuatorAutoConfiguration.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SpringContextTest {
 
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(HttpTraceActuatorAutoConfiguration.class));
+
     @Test
-    void whenSpringContextIsBootstrapped_thenNoExceptions() {
-        // no body
+    void defaultServiceBacksOff() {
+        this.contextRunner.withUserConfiguration(UserConfiguration.class).run((context) -> {
+            assertThat(context).hasSingleBean(HttpTraceActuatorAutoConfiguration.class);
+            assertThat(context).getBean("httpTraceRepository").isSameAs(context.getBean(HttpTraceRepository.class));
+        });
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    static class UserConfiguration {
+        @Bean
+        HttpTraceRepository httpTraceRepository() {
+            return new InMemoryHttpTraceRepository();
+        }
     }
 
 }
